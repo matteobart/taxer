c6dbc00f9da97f6f9d648f6f40be4525b612c626
Matteo Bartalini <MatteoBartalini1@gmail.com>
2024-10-20 17:26:42 -0700

    use generator + simplify transaction

--- transaction.py
+++ transaction.py
@@ -1,15 +1,22 @@
+from constants import TRANSACTION_SELL
+
+
 class Transaction:
-    def __init__(self, transaction_size, cost_basis, datetime):
+    def __init__(
+        self, transaction_size, cost_basis, datetime, transaction_type=TRANSACTION_SELL
+    ):
         self.transaction_size = transaction_size
         self.cost_basis = cost_basis
         self.datetime = datetime
+        self.transaction_type = transaction_type
 
     def __repr__(self):
-        return f"{self.datetime} size: {self.transaction_size} cost: {self.cost_basis}"
+        return f"{self.datetime} size: {self.transaction_size} cost: {self.cost_basis} type: {self.transaction_type}"
 
     def __eq__(self, transaction2):
         return (
             self.transaction_size == transaction2.transaction_size
             and self.cost_basis == transaction2.cost_basis
             and self.datetime == transaction2.datetime
+            and self.transaction_type == transaction2.transaction_type
         )
